553f3f0e6bb6f24cd892b21dad09140e175c41bd
Mark Fine <mark.fine@gmail.com>
2015-10-07 23:48:57 -0700

    Improved run.

--- examples/run.sh
+++ examples/run.sh
@@ -2,4 +2,19 @@
 
 set -e
 
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+    echo "$AWS_ACCESS_KEY_ID is not set"
+    exit 1
+fi
+
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+    echo "$AWS_SECRET_ACCESS_KEY"
+    exit 1
+fi
+
+stack build wolf --copy-bins
+
+wolf-register -c 'cfg/config.yaml' -p 'cfg/plan.yaml'
+wolf-execute -c 'cfg/config.yaml' -p 'cfg/plan.yaml' -i 'execute.json'
+
 docker-compose up
