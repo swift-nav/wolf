fe0c31d38f385dfab5eaa78b651f16e02ffc8487
Mark Fine <mark.fine@gmail.com>
2015-10-12 14:01:08 -0700

    Fix the warning on the script.

--- examples/run.sh
+++ examples/run.sh
@@ -3,12 +3,12 @@
 set -e
 
 if [ -z "$AWS_ACCESS_KEY_ID" ]; then
-    echo "$AWS_ACCESS_KEY_ID is not set"
+    echo "AWS_ACCESS_KEY_ID is not set"
     exit 1
 fi
 
 if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
-    echo "$AWS_SECRET_ACCESS_KEY"
+    echo "AWS_SECRET_ACCESS_KEY is not set"
     exit 1
 fi
 
