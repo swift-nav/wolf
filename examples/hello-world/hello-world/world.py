7085b185674d5e93c9bc564ba45bb1de3da4fac0
Mark Fine <mark.fine@gmail.com>
2015-12-01 14:21:46 -0800

    fix up demo scripts.

--- examples/hello-world/hello-world/world.py
+++ examples/hello-world/hello-world/world.py
@@ -3,31 +3,35 @@
 import copy
 import json
 import os
+import sys
+
+def printerr(*args):
+  [sys.stderr.write(str(s)) for s in args]
 
 def main():
-  with open('/app/data/control.json', 'r') as control_file:
+  with open('./data/control.json', 'r') as control_file:
     control_data = json.load(control_file)
-    print '\n', control_data, '\n'
+    printerr('\n', control_data, '\n')
 
-  with open('/app/data/input.json', 'r') as input_file:
+  with open('./data/input.json', 'r') as input_file:
     input_data = json.load(input_file)
-    print '\n', input_data, '\n'
+    printerr('\n', input_data, '\n')
 
-  inputs = os.listdir('/app/store/input')
-  print '\n', inputs, '\n'
+  inputs = os.listdir('./store/input')
+  printerr('\n', inputs, '\n')
 
-  print '\n', "world worker", '\n'
+  printerr('\n', "world worker", '\n')
   output_data = copy.deepcopy(input_data)
   output_data['step-2'] = 'world'
   store_data = copy.deepcopy(output_data)
   store_data['storage'] = 'true'
 
-  print '\n', output_data, '\n'
-  with open('/app/data/output.json', 'w') as output_file:
+  printerr('\n', output_data, '\n')
+  with open('./data/output.json', 'w') as output_file:
     json.dump(output_data, output_file)
 
-  print '\n', store_data, '\n'
-  with open('/app/store/world.json', 'w') as store_file:
+  printerr('\n', store_data, '\n')
+  with open('./store/world.json', 'w') as store_file:
     json.dump(store_data, store_file)
 
 if __name__ == "__main__":
