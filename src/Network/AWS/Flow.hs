86b86dd6b7cd3ab5669cbf2de47fdcfcb8afe497
Mark Fine <mark.fine@gmail.com>
2016-11-10 09:22:47 -0800

    Turn off the execssive logging.

--- src/Network/AWS/Flow.hs
+++ src/Network/AWS/Flow.hs
@@ -96,13 +96,11 @@ act queue action = do
   maybe_ token' $ \token ->
     maybe_ uid' $ \uid -> do
       logInfo' $ sformat ("event=act-begin uid=" % stext) uid
-      maybe_ input $ logDebug' . sformat ("event=act-input " % stext)
       keys <- listObjectsAction uid
       unless (null keys) $ logInfo' $ sformat ("event=list-blobs uid=" % stext) uid
       blobs <- forM keys $ getObjectAction uid
       unless (null blobs) $ logInfo' $ sformat ("event=blobs uid=" % stext) uid
       (output, artifacts, e) <- action uid input blobs
-      maybe_ output $ logDebug' . sformat ("event=act-output " % stext)
       logInfo' $ sformat ("event=act-finish uid=" % stext) uid
       forM_ artifacts $ putObjectAction uid
       unless (null artifacts) $ logInfo' $ sformat ("event=artifacts uid=" % stext) uid
